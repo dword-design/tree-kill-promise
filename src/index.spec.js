2d1c78b8b3629872406cbdac8f95097f257699cb
dword <sebastianlandw@gmail.com>
2020-07-04 18:59:37 +0000

    fix: fixes after upgrades

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,22 +1,27 @@
-import withLocalTmpDir from 'with-local-tmp-dir'
-import execa from 'execa'
 import { endent } from '@dword-design/functions'
-import { outputFile, chmod } from 'fs-extra'
+import execa from 'execa'
+import { chmod, outputFile } from 'fs-extra'
 import portReady from 'port-ready'
+import withLocalTmpDir from 'with-local-tmp-dir'
+
 import kill from '.'
 
 export default {
-  valid: () => withLocalTmpDir(async () => {
-    await outputFile('cli.js', endent`
+  valid: () =>
+    withLocalTmpDir(async () => {
+      await outputFile(
+        'cli.js',
+        endent`
       #!/usr/bin/env node
 
       import http from 'http'
 
       http.createServer().listen(3000)
-    `)
-    await chmod('cli.js', '755')
-    const childProcess = execa.command('./cli.js')
-    await portReady(3000)
-    await kill(childProcess.pid)
-  }),
+    `
+      )
+      await chmod('cli.js', '755')
+      const childProcess = execa.command('./cli.js')
+      await portReady(3000)
+      await kill(childProcess.pid)
+    }),
 }
